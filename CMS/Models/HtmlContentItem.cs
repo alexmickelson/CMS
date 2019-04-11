e4cd2d11fe41101f4c0cdfe2cf46972efb016ba2
alexmickelson96 <alexmickelson96@gmail.com>
2019-04-11 10:29:09 -0600

    frontend posts to controller

---
+++ CMS/Models/HtmlContentItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Models
+{
+    public class HtmlContentItem
+    {
+        public string ClassList { get; set; }
+        public string StyleContent { get; set; }
+
+    }
+}
